06eed943fda8ee83c1f1eabc2c0621c14bf7e19c
HuyKDTVN <huy.ld.352@dtvn.kyocera.com>
2026-03-11 09:56:56 +0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll("button");
-buttons.forEach(btn => {
-  btn.addEventListener("click", function () {
-    const id = this.dataset.id;
-    this.classList.add("active");
-    navigator.clipboard.writeText(id);
-  });
-});
